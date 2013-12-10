d0346b7e6ce72b87e852adac67891b76d371d14b
vanhouc <vanhouc@mail.nmc.edu>
2013-12-10 11:32:04 -0500

    Linted, added paragraph function

--- Review.js
+++ Review.js
@@ -1,20 +1,36 @@
 //Script file for CIT190 Review
-$(document).ready(function() {
-	'use strict'
-	$('#first').click(function(){
-		reverse();
-	});
-});
-function reverse() {
-	var input = document.getElementById('userVal').value.trim();
-	var output = '';
-	for (var i=1;i < input.length + 1;i++) {
-		var x = input[input.length - i]
+function reverse(domObj) {
+	'use strict';
+	var input = domObj.value.trim(),
+	output = '',
+	i,
+	x;
+	for (i=1;i < input.length + 1;i+=1) {
+		x = input[input.length - i];
 		if (x ==='undefined')
 		{
-			output += '#'
+			output += '#';
 		}
 		output += x;
 	}
 	document.getElementById('userVal').value = output;
 }
+function makeParagraph(domObj) {
+	var i,x,input = domObj.value.trim(),output = '', paragraph = '';
+	for (i=0;i<20;i+=1) {
+		paragraph += input;
+	}
+	for (i=0;i<4;i+=1) {
+		output += '<p>' + paragraph + '</p>';
+	}
+	return output;
+}
+$(document).ready(function () {
+	'use strict';
+	$('#first').click(function () {
+		reverse(document.getElementById('userVal'));
+	});
+	$('#second').click(function () {
+		$('#formControls').after(makeParagraph(document.getElementById('userVal')));
+	});
+});
